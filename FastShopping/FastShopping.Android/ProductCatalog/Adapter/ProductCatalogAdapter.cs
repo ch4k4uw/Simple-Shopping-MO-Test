444d957a084cef18c16966ca5f02de9a83c6bb3d
Pedro Motta <garra_e@hotmail.com>
2018-06-05 00:02:45 -0300

    Added image downloader and cache system for that.

--- FastShopping/FastShopping.Android/ProductCatalog/Adapter/ProductCatalogAdapter.cs
+++ FastShopping/FastShopping.Android/ProductCatalog/Adapter/ProductCatalogAdapter.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 using System.Text;
 
 using Android.App;
@@ -12,6 +15,7 @@ using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
 using Application.Dto.Result;
+using RestSharp;
 
 namespace FastShopping.Droid.ProductCatalog.Adapter
 {
@@ -49,6 +53,8 @@ namespace FastShopping.Droid.ProductCatalog.Adapter
             pcHolder.Quantity.Text = $"{item.Quantity}";
             pcHolder.ChangeCheckState(item.IsFavorite);
 
+            DownloadImage(pcHolder.Img, item.Photo);
+
         }
 
         public void OnClick(View v)
@@ -69,6 +75,108 @@ namespace FastShopping.Droid.ProductCatalog.Adapter
         {
             var view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.product_catalog_item, parent, false);
             return new ProductCatalogViewHolder(view, this);
+        }
+
+        private IDictionary<string, ImageView> pendingDownloadsImgIndex = new Dictionary<string, ImageView>();
+        private IDictionary<string, IDisposable> pendingDownloadsSubscriptionIndex = new Dictionary<string, IDisposable>();
+        private void DownloadImage(ImageView imageView, string url)
+        {
+            var oldUrl = (string)imageView.Tag;
+            if(oldUrl != null)
+            {
+                pendingDownloadsSubscriptionIndex[oldUrl].Dispose();
+                pendingDownloadsSubscriptionIndex.Remove(oldUrl);
+                pendingDownloadsImgIndex.Remove(oldUrl);
+
+                imageView.Tag = null;
+            }
+
+            imageView.Tag = url;
+            pendingDownloadsImgIndex.Add(url, imageView);
+
+            imageView.SetImageDrawable(null);
+            pendingDownloadsSubscriptionIndex.Add(url,
+                Observable
+                    .Start(() =>
+                    {
+                        var localUri = GetURIFromCache(url);
+                        var exists = localUri != "" && File.Exists(localUri);
+                        if (localUri == "" || !exists)
+                        {
+                            if(localUri != "" && !exists)
+                            {
+                                RemoveFromCache(url);
+                            }
+
+                            var uri = new Uri(url);
+
+                            RestClient restClient = new RestClient(url);
+                            var fileBytes = restClient.DownloadData(new RestRequest(Method.GET));
+
+                            if (fileBytes != null)
+                            {
+                                var path = Path.Combine(context.CacheDir.AbsolutePath, Path.GetFileName(uri.LocalPath));
+                                File.WriteAllBytes(path, fileBytes);
+                                var path2 = Path.Combine(context.GetExternalFilesDir(null).AbsolutePath, Path.GetFileName(uri.LocalPath));
+                                File.WriteAllBytes(path2, fileBytes);
+
+                                PersistCache(url, path);
+
+                                localUri = path;
+                            }
+                        }
+
+                        return localUri;
+                    })
+                    .ObserveOn(ReactiveUI.HandlerScheduler.MainThreadScheduler)
+                    .SubscribeOn(DefaultScheduler.Instance/*NewThreadScheduler.Default*/)
+                    .Subscribe(localUri => 
+                    {
+                        pendingDownloadsSubscriptionIndex.Remove(url);
+                        pendingDownloadsImgIndex.Remove(url);
+
+                        imageView.Tag = null;
+
+                        imageView.SetImageURI(Android.Net.Uri.Parse(localUri));
+                    }, err => {
+                        pendingDownloadsSubscriptionIndex.Remove(url);
+                        pendingDownloadsImgIndex.Remove(url);
+
+                        imageView.Tag = null;
+                    })
+                );
+        }
+
+        private void PersistCache(string url, string path)
+        {
+            lock(context)
+            {
+                ISharedPreferences shp = context.GetSharedPreferences("image_cache", FileCreationMode.Private);
+                shp.Edit()
+                    .PutString(url, path)
+                    .Commit();
+            }
+        }
+
+        private void RemoveFromCache(string url)
+        {
+            lock (context)
+            {
+                ISharedPreferences shp = context.GetSharedPreferences("image_cache", FileCreationMode.Private);
+                shp.Edit()
+                    .Remove(url)
+                    .Commit();
+
+            }
+        }
+
+        private string GetURIFromCache(string url)
+        {
+            lock (context)
+            {
+                ISharedPreferences shp = context.GetSharedPreferences("image_cache", FileCreationMode.Private);
+                return shp.GetString(url, "");
+            }
         }
     }
 }
